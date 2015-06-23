39bd83b5a2192d82f7e8dfb5fc37b8ca88b1e0fd
Johnny Richardson <john@thejohnshow.com>
2015-06-23 17:58:33 -0400

    Resolved a ton on YAML content issues, updated gameData action to allow periods in file names (though I discourage this).

--- server/debug.sh
+++ server/debug.sh
@@ -3,11 +3,11 @@ NC='\033[0m' # No Color
 
 
 printf "\n${COLOR}@@@@@@@@@@@@@@\nEngagement Lab World Bank API debug\n@@@@@@@@@@@@@@\n\nYou may also want to run (in seperate tabs):\n"
-printf "\nforever /usr/local/lib/node_modules/mongo-express/app\n"
+printf "\nforever start /usr/local/lib/node_modules/mongo-express/app.js\n"
 printf "\n./node_modules/.bin/node-inspector --web-port=8080\n\n"
 printf "DO NOT RUN THIS IN PRODUCTION!!\n\n${NC}"
 
-sleep 5
+sleep 3
 
 node --debug ./node_modules/.bin/actionhero start
 
