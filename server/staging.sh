ef4aa6017e03906b6debb5284bbfea8385c1acdc
Johnny Richardson <john@thejohnshow.com>
2015-07-01 11:49:04 -0400

    Updated staging script and implemented staging mongo connection initializer.

--- server/staging.sh
+++ server/staging.sh
@@ -1,7 +1,17 @@
+# World Bank API
+# Created by Engagement Lab, 2015
+# ==============
+#  staging.sh
+#  Staging server upload script
+
+#  Created by Johnny Richardson on 7/1/15.
+# ==============
+
+
 COLOR='\033[1;35m'
 NC='\033[0m' # No Color
 
-printf "\n${COLOR}@@@@@@@@@@@@@@\nEngagement Lab World Bank API Staging Upload\n@@@@@@@@@@@@@@\n\n${NC}"
+printf "\n${COLOR}@@@@@@@@@@@@@@\nEngagement Lab API Staging Upload\n@@@@@@@@@@@@@@\n\n${NC}"
 
 printf "\n>>>> Switching to staging branch\n"
 
@@ -9,18 +19,21 @@ git checkout staging
 
 printf "\n>>>> Merging master branch into staging working tree\n"
 
-printf "\n>>>> Copying /content to server root\n"
+git rebase master
 
-cp -R ../content .
+printf "\n>>>> Copying /content to server root for Heroku\n"
 
-printf "\n>>>> Adding content changes and committing \n"
+cp -R content server
 
-# git add .
-# git commit -m "Made staging commit from master head ($(git rev-parse HEAD))"
+printf "\n>>>> Adding content changes and committing to branch from master head $(git rev-parse HEAD) \n"
+
+git add .
+git commit -m "Made staging commit from master head ($(git rev-parse HEAD))"
+
+printf "\n>>>> Setting NODE_ENV to staging and pushing to Heroku \n"
 
 heroku config:set NODE_ENV=staging
 
 git subtree push --prefix server heroku master
 
-# export NODE_ENV=staging
-# node ./node_modules/.bin/actionhero start
+printf "\n****** DONE ******* \n"
