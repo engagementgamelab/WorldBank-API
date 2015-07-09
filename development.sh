d45f39e1c0241bfbbbebb43f80edb67a6b580ed8
Johnny Richardson <john@thejohnshow.com>
2015-07-09 12:33:57 -0400

    Added development automation script.

---
+++ development.sh
@@ -0,0 +1,21 @@
+# World Bank API
+# Created by Engagement Lab, 2015
+# ==============
+#  development.sh
+#  Development server automation script
+
+#  Created by Johnny Richardson on 7/9/15.
+# ==============
+
+printf "\n${COLOR}@@@@@@@@@@@@@@\nEngagement Lab API Dev Automation\n@@@@@@@@@@@@@@\n\n${NC}"
+
+printf "\n>>>> Copying /content to server root for Heroku usage\n"
+
+rm -R server/content
+cp -R content server
+
+wait
+
+printf "\n>>>> Adding content to server root for git push \n"
+
+git add .
