45047fdb64f3dac744bbd777b4166d9ca924e990
Johnny Richardson <john@thejohnshow.com>
2015-09-16 16:06:37 -0400

    Redis config update.

--- server/config/redis.js
+++ server/config/redis.js
@@ -39,20 +39,24 @@ exports.default = {
   }
 }
 
-exports.test = { 
+exports.production = {
   redis: function(api){
-    var package = 'fakeredis';
-    if(process.env.FAKEREDIS === 'false'){
-      package = 'redis';
+    var redisDetails = {
+      // Which channel to use on redis pub/sub for RPC communication
+      channel: 'actionhero',
+      // How long to wait for an RPC call before considering it a failure 
+      rpcTimeout: 5000, 
+      // which redis package should you ise?
+      package: 'redis'
     }
 
-    return {
-      'package': package,
-      'host': '127.0.0.1',
-      'port': 6379,
-      'password': null,
-      'options': null,
-      'database': 2
-    }
+    redisDetails.package  = 'redis';
+    redisDetails.host     = process.env.REDIS_HOST || '127.0.0.1';
+    redisDetails.port     = process.env.REDIS_PORT || 6379;
+    redisDetails.password = process.env.REDIS_PASS || null;
+    redisDetails.database = process.env.REDIS_DB   || 0;
+    redisDetails.options  = null;
+
+    return redisDetails;
   }
 }
